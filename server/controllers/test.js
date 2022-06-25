c24e0f8f20b0afb3afbab6ba99665395ec6493aa
DJainTrakGene <dixit.jain@trakgene.com>
2022-06-25 21:47:47 +0930

    creating initial setup

---
+++ server/controllers/test.js
@@ -0,0 +1,12 @@
+exports.getTest = async (req, res) => {
+  res.status(200).json({
+    message: "Test API is working!",
+  });
+};
+
+exports.getListings = async (req, res) => {
+  res.status(200).json({
+    //TODO
+    message: "hmm",
+  });
+};
